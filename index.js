d369157e5362e8ce4a1a6c103b411c0383d3271d
Matthew Stead <mattstead6@gmail.com>
2022-03-09 16:49:29 -0500

    search bar starting to work

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 //could assign newNewsObject to a global variable
+emptyArticleArray = []
+console.log('emptyArticleArray', emptyArticleArray)
+allKeywords = []
+console.log(allKeywords)
+
+
 const form = document.querySelector('form')
 form.addEventListener('keyup', (e) => {
 const searchString = e.target.value
 console.log(searchString)
-story.keywords.includes(searchString)
-});
+const filterNewsArray = emptyArticleArray.filter(newsStory => {
+  return newsStory.keywords.includes(searchString)
 
+})
+aftersearch(filterNewsArray)
+})
+
+function aftersearch(searchArray){
+    console.log(searchArray)
+}
+
+// send to a new function that removes previous articles and replaces with this new filteres news array
+
+// console.log(filterAllKeywords)
+
+
+
+// const keyword = allKeywords.filter(word => {
    
-// const filtered
+//         if (word == searchString) {
+//             console.log('keyword exists')
+//         } else {
+//             console.log('no match')
+//         }
+      
+//     })
+    // if (keyword.includes(searchString)){
+    //     console.log('yay')
+    // }
+    // console.log(keyword.toString())
 
-// console.log(foxNewsKeywords)
-// let deepKeywordsObj = {}
-// console.log(deepKeywordsObj)
-// console.log(deepKeywordsArray)
-let makeTheArticle = {}
-console.log(makeTheArticle)
+// console.log('HERE',typeof keyword)
+    // if (allKeywords[0].includes(searchString)){
+    //     console.log('hi')
+    // }
+    // else{
+    //     console.log('did not work')
+    // }
+
+// console.log(searchString)
+
+// const filteredKeywords = emptyArticleArray.filter(array => {
+//     // console.log(array.keywords)
+//     console.log(array.keywords)
+//     if(searchString == allKeywords){
+//         // makeArticle(filteredKeywords)
+//         console.log('hi')
+//     }
+// })
+
+
+    
+
+
+
+
     
 
 
@@ -38,18 +88,18 @@ function makeArticle(story){
     aTag.appendChild(imgTag)
     articleTag.appendChild(aTag)
     h2Tag.appendChild(document.createTextNode(story.title))
-    let urlAttribute = document.createAttribute('href')
-    urlAttribute.value = story.url
-    aTag.setAttributeNode(urlAttribute)
+    let href = document.createAttribute('href')
+    href.value = story.url
+    aTag.setAttributeNode(href)
     aTag.appendChild(imgTag)
     let src = document.createAttribute('src')
     src.value = story.image
     imgTag.setAttributeNode(src)
-    // let allKeywords = story.keywords
+   allKeywords.push(story.keywords[0])
     // deepKeywordsArray.push(allKeywords)
     // deepKeywordsObj.push(story)
     // filterFunction(story)
-    console.log(story)
+    emptyArticleArray.push(story)
     return articleTag
     
  }
