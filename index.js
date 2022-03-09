df1a5b64f2c7ed10166d463305a0bd13a3301c1f
Adam <adam.crespo101@outlook.com>
2022-03-09 11:43:33 -0500

    changes made

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+document.addEventListener('DOMContentLoaded', (e) => {
 
+    const form = document.getElementById('comment-section')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.getElementById('textarea').value
+         const newComment = document.createElement('p');
+        newComment.textContent = input;
+        const container = document.getElementById('comment-container')
+        container.appendChild(newComment)
+        const deleteBtn = document.createElement('button')
+        deleteBtn.innerText = 'x'
+        deleteBtn.addEventListener('click', (e) => {
+            e.target.parentNode.remove();
+            
+        })
+        newComment.append(deleteBtn)
+        })
+    
+    })
+
+    renderFox();
+    renderCnn();
+    renderBbc();
+    renderTrend();
+   
+    
+    
+
+
+    
+   
+   
+   
    const left = document.getElementById('leftside')
    const right = document.getElementById('rightside')
    
+
 
    function renderFox (news){
    fetch('https://newsapi.org/v2/top-headlines?sources=fox-news&apiKey=2a61e83cb0f542c7aa6a93568b9717d5')
     .then(res => res.json())
     .then(news => {
+        const button = document.getElementById('fox') 
+        button.addEventListener('click', () => {
         let newsArr = news.articles 
         let i = newsArr[Math.floor(Math.random()* 10)]; 
         const author = document.createElement('h3')
@@ -19,34 +55,98 @@
         desc.textContent = i.description
         const img = document.createElement('img')
         img.src = i.urlToImage
+        img.style.width = '600px';
+        img.style.height = '400px';
+        const link = document.createElement('a')
+            link.textContent = 'Link to Article'
+            link.href = i.url
+        desc.append(link)
         right.append(author, title, desc, img, date)
-    }
+    })}
 )}
 
     function renderCnn (news){
         fetch('https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=2a61e83cb0f542c7aa6a93568b9717d5')
          .then(res => res.json())
          .then(news => {
+            const button = document.getElementById('cnn') 
+            button.addEventListener('click', () => {
             let newsArr = news.articles
-            //  newsArr.push(news.articles)
-            // console.log('fetch', newsArr)
+             let i = newsArr[Math.floor(Math.random()* 10)]; //iterates through object to produce a random 
+            console.log(i)
+            const title = document.createElement('h4')
+            title.textContent = i.title
+            const date = document.createElement('h5')
+            date.textContent = i.publishedAt
+            const desc = document.createElement('p')
+            desc.textContent = i.description
+            const link = document.createElement('a')
+            link.textContent = 'Link to Article'
+            link.href = i.url
+            const img = document.createElement('img')
+            img.src = i.urlToImage
+            img.style.width = '600px';
+            img.style.height = '400px';
+            desc.append(link)
+            left.append(title, desc, img, date)
+             
+             console.log(news)
+            })
+        })
+    }
+        function renderBbc (news) {
+        const button = document.getElementById('bbc') 
+        button.addEventListener('click', () => {
+               fetch('https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=2a61e83cb0f542c7aa6a93568b9717d5')
+               .then(res => res.json())
+               .then(news => {
+                let newsArr = news.articles                
+                 let i = newsArr[Math.floor(Math.random()* 10)]; //iterates through object to produce a random 
+                console.log(i)
+               const title = document.createElement('h4')
+                title.textContent = i.title
+                const desc = document.createElement('p')
+                desc.textContent = i.description
+                const link = document.createElement('a')
+                link.textContent = 'Link to Article'
+                link.href = i.url
+                const img = document.createElement('img')
+                img.src = i.urlToImage
+                img.style.width = '600px';
+                img.style.height = '400px';
+                desc.append(link)
+                left.append(title, desc, img)
+               })
+
+            })     
+        }     
+
+function renderTrend (news) {
+    const button = document.getElementById('trending') 
+    button.addEventListener('click', () => {
+           fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=2a61e83cb0f542c7aa6a93568b9717d5')
+           .then(res => res.json())
+           .then(news => {
+               console.log(news)
+            let newsArr = news.articles                
              let i = newsArr[Math.floor(Math.random()* 10)]; //iterates through object to produce a random 
             console.log(i)
             const title = document.createElement('h4')
             title.textContent = i.title
             const desc = document.createElement('p')
             desc.textContent = i.description
+            const link = document.createElement('a')
+            link.textContent = 'Link to Article'
+            link.href = i.url
             const img = document.createElement('img')
             img.src = i.urlToImage
-            img.setAttribute = ('width', '400');
-            img.setAttribute = ('height', '400px');
-            left.append(title, desc, img)
-             
-             console.log(news)
-             })
-         }
-document.addEventListener('DOMContentLoaded', () => {
-    renderFox();
-    renderCnn();
-})
-    
+            img.style.width = '600px';
+            img.style.height = '400px';
+            desc.append(link)
+            right.append(title, desc, img)
+           })
+        })     
+    }
+
+
+
